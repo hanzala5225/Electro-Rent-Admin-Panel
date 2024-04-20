2c347c34298b710966241effaf73b124295c1004
Hanzala Ahmed Malik <hanzalaahmed6718@gmail.com>
2024-04-21 02:27:58 +0500

    FETCHING THE CATEGORIES FROM FIREBASE TO ADD PRODUCT SCREEN - FINAL

--- lib/widgets/Dropdown-Categories-Widget.dart
+++ lib/widgets/Dropdown-Categories-Widget.dart
@@ -5,59 +5,59 @@ import 'package:get/get.dart';
 import '../controllers/Category-Dropdown-Controller.dart';
 
 class DropDownCategoriesWidget extends StatelessWidget {
-  const DropDownCategoriesWidget({super.key});
+  const DropDownCategoriesWidget({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return GetBuilder<CategoryDropDownController>(
       init: CategoryDropDownController(),
-        builder:
-        (categoryDropDownController) {
-        return Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Card(
-                elevation: 10,
-                child: Padding(padding: EdgeInsets.all(8.0),
-                  child: DropdownButton<String>(
-                    value: categoryDropDownController.selectedCategoryId?.value,
-                    items: categoryDropDownController
-                        .categories
-                        .map((category) {
-                          return DropdownMenuItem<String>(
-                              child: Row(
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  CircleAvatar(
-                                    backgroundImage: NetworkImage(category["categoryImg"].toString()),
-                                  ),
-                                  SizedBox(
-                                    width: 20.0,
-                                  ),
-                                  Text(category['categoryName']),
-                                ],
+      builder: (categoryDropDownController) {
+        return SingleChildScrollView( // Wrap with SingleChildScrollView
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Card(
+                  elevation: 10,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: DropdownButton<String>(
+                      value: categoryDropDownController.selectedCategoryId?.value,
+                      items: categoryDropDownController.categories
+                          .map<DropdownMenuItem<String>>((category) {
+                        return DropdownMenuItem<String>(
+                          value: category['categoryId'], // Assuming 'categoryId' is the unique identifier
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                  category["categoryImg"].toString(),
+                                ),
                               ),
-                          );
-                        },
-                    )
-                        .toList(),
-                    onChanged: (String? selectedValue) async {
-                      categoryDropDownController.setSelectedCategory(selectedValue);
-                      String? categoryName = await categoryDropDownController.getCategoryName(selectedValue);
-
-                      categoryDropDownController.setSelectedCategoryName(categoryName)
-                    },
-                    hint: Text("Select Product Category"),
-                    isExpanded: true,
-                    elevation: 10,
-                    underline: SizedBox.shrink(),
+                              SizedBox(
+                                width: 20.0,
+                              ),
+                              Text(category['categoryName']),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? selectedValue) async {
+                        categoryDropDownController.setSelectedCategory(selectedValue);
+                      },
+                      hint: Text("Select Product Category"),
+                      isExpanded: true,
+                      elevation: 10,
+                      underline: SizedBox.shrink(),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
-        });
+      },
+    );
   }
 }
